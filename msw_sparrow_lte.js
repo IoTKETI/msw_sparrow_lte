e9d96428f4f116e3e475e5b6d13b83f0fc625f78
Daphnis <iyahn@keti.re.kr>
2020-09-05 20:00:25 +0900

    optimize code - msw standard

--- msw_sparrow_lte.js
+++ msw_sparrow_lte.js
@@ -205,7 +205,7 @@ function msw_mqtt_connect(broker_ip, port) {
 function on_receive_from_muv(topic, str_message) {
     console.log('[' + topic + '] ' + str_message);
 
-    parseControlMission(str_message, function (obj_muv_control) {
+    parseControlMission(topic, str_message, function (obj_muv_control) {
         var topic_arr = topic.split('/');
         var _topic = '/MUV/control/' + config.lib[0].name + '/' + topic_arr[topic_arr.length - 1];
         msw_mqtt_client.publish(_topic, JSON.stringify(obj_muv_control));
@@ -215,7 +215,7 @@ function on_receive_from_muv(topic, str_message) {
 function on_receive_from_lib(topic, str_message) {
     console.log('[' + topic + '] ' + str_message);
 
-    parseDataMission(str_message, function (obj_lib_data) {
+    parseDataMission(topic, str_message, function (obj_lib_data) {
         var topic_arr = topic.split('/');
         var data_topic = '/Mobius/' + config.gcs + '/Mission_Data/' + config.drone + '/' + config.name + '/' + topic_arr[topic_arr.length-1];
         msw_mqtt_client.publish(data_topic + config.sortie_name, JSON.stringify(obj_lib_data));
@@ -236,7 +236,7 @@ setTimeout(init, 1000);
 
 // 유저 디파인 미션 소프트웨어 기능
 
-function parseDataMission(str_message, callback) {
+function parseDataMission(topic, str_message, callback) {
     var obj_lib_data = {};
     try {
         obj_lib_data = JSON.parse(str_message);
@@ -251,7 +251,7 @@ function parseDataMission(str_message, callback) {
     }
 }
 
-function parseControlMission(str_message, callback) {
+function parseControlMission(topic, str_message, callback) {
     var obj_muv_control = {};
 
     try {
